9c775dd27de1fbe84c373951ca3331f41e86df78
Yassine Eluharani <eluharani.yassine@gmail.com>
2024-01-17 15:33:57 +0100

    Catppuccin colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,16 @@
 return {
-	{
-		"rose-pine/neovim",
-		lazy = false,
-		name = "rose-pine",
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("rose-pine")
-		end,
-	},
+  -- {
+  --   "rose-pine/neovim",
+  --   lazy = false,
+  --   name = "rose-pine",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("rose-pine")
+  --   end,
+  -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  }
 }
